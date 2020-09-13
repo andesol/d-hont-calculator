bfd77fe1b6ab079744fbb11fc105bdd0aa4c7f27
Andreu <anddessol@gmail.com>
2020-09-13 13:49:48 +0200

    First commit

--- lab.js
+++
@@ -1,7 +0,0 @@
-const assignSeats = require("./src/utils/assignSeats");
-const seats = 7;
-const votesPerParty = [340000, 280000, 160000, 60000, 15000];
-
-// const result = [3, 3, 2, 0, 0];
-const results = assignSeats(seats, votesPerParty);
-console.log(results);
